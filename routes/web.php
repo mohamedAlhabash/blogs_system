6dae1b849f9663fb0f75696a8ed8b69088b9bebd
mohamedAlhabash <mohamedwaha2000@gmail.com>
2022-08-05 12:10:56 +0300

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,14 @@
 <?php
 
-use Frontend\Auth\LoginController;
-use Illuminate\Support\Facades\Auth;
-use Frontend\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use Frontend\Auth\VerificationController;
-use Frontend\Auth\ResetPasswordController;
-use Frontend\Auth\ForgotPasswordController;
+// use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Frontend\IndexController;
+use App\Http\Controllers\Frontend\Auth\LoginController;
+// use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Frontend\Auth\RegisterController;
+use App\Http\Controllers\Frontend\Auth\VerificationController;
+use App\Http\Controllers\Frontend\Auth\ResetPasswordController;
+use App\Http\Controllers\Frontend\Auth\ForgotPasswordController;
 // use App\Http\Controllers\Backend\IndexController;
 
 /*
@@ -25,22 +25,24 @@ use App\Http\Controllers\Frontend\IndexController;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
- Route::get('/',                                [IndexController::class, 'index']);
+
+ Route::get('/',                                [IndexController::class, 'index'])->name('frontend.index');
 
 // Auth::routes();
 
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 //Authentication Routes...
-Route::get('/login',                            [LoginController::class ,'showLoginForm'])->name('frontend.show_login_form');
-Route::post('login',                            [LoginController::class ,'login'])->name('frontend.login');
-Route::post('logout',                           [LoginController::class,'logout'])->name('frontend.logout');
-Route::get('register',                          [RegisterController::class,'showRegistrationForm'])->name('frontend.show_register_form');
-Route::post('register',                         [RegisterController::class ,'register'])->name('frontend.register');
+Route::get('/login',                            [LoginController::class ,'showLoginForm'])->name('frontend.login');
+Route::post('login',                            [LoginController::class ,'login']);
+Route::post('logout',                           [LoginController::class,'logout']);
+Route::get('register',                          [RegisterController::class,'showRegistrationForm']);
+Route::post('register',                         [RegisterController::class ,'register']);
 Route::get('password/reset',                    [ForgotPasswordController::class,'showLinkRequestForm'])->name('password.request');
-Route::post('password/email',                   [ForgotPasswordController::class,'sendResetLinkEmail'])->name('password.email');
-Route::get('password/reset/{token}',            [ResetPasswordController::class,'showResetForm'])->name('password.reset');
-Route::post('password/reset',                   [ResetPasswordController::class,'reset'])->name('password.update');
-Route::get('email/verify',                      [VerificationController::class,'show'])->name('verification.notice');
-Route::get('/email/verify/{id}/{hash}',         [VerificationController::class,'verify'])->name('verification.verify')->name('verification.resend');
+Route::post('password/email',                   [ForgotPasswordController::class,'sendResetLinkEmail']);
+Route::get('password/reset/{token}',            [ResetPasswordController::class,'showResetForm']);
+Route::post('password/reset',                   [ResetPasswordController::class,'reset']);
+Route::get('email/verify',                      [VerificationController::class,'show']);
+Route::get('/email/verify/{id}/{hash}',         [VerificationController::class,'verify']);
 Route::post('email/resend',                     [VerificationController::class,'resend']);
+
